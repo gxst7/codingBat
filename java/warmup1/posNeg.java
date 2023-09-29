61020acca278fa5c781d4fdd72a85deea95503b6
Alexander Gostilo <alexandergostilo@gmail.com>
2023-09-30 00:05:04 +0300

    add posNeg problem from warmup1

---
+++ java/warmup1/posNeg.java
@@ -0,0 +1,25 @@
+package java.warmup1;
+
+/**
+ * Warmup-1 > posNeg 
+ * 
+ * Given 2 int values, return true if one is negative and one is positive. Except if the 
+ * parameter "negative" is true, then return true only if both are negative.
+ * 
+ * posNeg(1, -1, false) → true
+ * posNeg(-1, 1, false) → true
+ * posNeg(-4, -5, true) → true
+ */
+
+public class posNeg {
+    public boolean posNeg(int a, int b, boolean negative) {
+        switch(String.valueOf(negative)) {
+          case "true":
+            return (a < 0 && b < 0);
+          case "false":
+            return (a < 0 && b > 0) || (a > 0 && b < 0);
+          default:
+            return false;
+        }
+    }    
+}
