14a9c389f17ed9780fe738aa41d29a301bf806f2
Alexander Gostilo <alexandergostilo@gmail.com>
2023-09-29 23:58:15 +0300

    add nearHundred problem from warmup1

---
+++ java/warmup1/nearHundred.java
@@ -0,0 +1,18 @@
+package java.warmup1;
+
+/**
+ * Warmup-1 > nearHundred 
+ * 
+ * Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes 
+ * the absolute value of a number.
+ * 
+ * nearHundred(93) → true
+ * nearHundred(90) → true
+ * nearHundred(89) → false
+ */
+
+public class nearHundred {
+    public boolean nearHundred(int n) {
+        return Math.abs(n - 100) <= 10 || Math.abs(n - 200) <= 10;
+    }
+}
