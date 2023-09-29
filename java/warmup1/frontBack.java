e07d5172f568aa3ac3a4c317a6c4a5f65b5f96e4
Alexander Gostilo <alexandergostilo@gmail.com>
2023-09-30 01:29:43 +0300

    add frontback problem

---
+++ java/warmup1/frontBack.java
@@ -0,0 +1,17 @@
+package java.warmup1;
+
+/**
+ * Warmup-1 > frontBack 
+ * 
+ * Given a string, return a new string where the first and last chars have been exchanged.
+ * 
+ * frontBack("code") → "eodc"
+ * frontBack("a") → "a"
+ * frontBack("ab") → "ba"
+ */
+
+public class frontBack {
+    public String frontBack(String str) {
+        return str.length() < 2 ? str : str.charAt(str.length()-1) + str.substring(1, str.length()-1) + str.charAt(0);
+    }
+}
