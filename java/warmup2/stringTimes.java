0a046f4f678896f0f7647c76ce5853228b597b5e
Alexander Gostilo <alexandergostilo@gmail.com>
2023-09-29 21:21:14 +0300

    add stringTimes problem from warmUp-2

---
+++ java/warmup2/stringTimes.java
@@ -0,0 +1,22 @@
+package java.warmup2;
+
+/**
+ * Warmup-2 > stringTimes 
+ * 
+ * Given a string and a non-negative int n, return a larger string that is n copies of the 
+ * original string.
+ * 
+ * stringTimes("Hi", 2) → "HiHi"
+ * stringTimes("Hi", 3) → "HiHiHi"
+ * stringTimes("Hi", 1) → "Hi"
+ */
+
+public class stringTimes {
+    public String stringTimes(String str, int n) {
+        String res = "";
+        for (int i = 0; i < n; i++) {
+          res += str;
+        }
+        return res;
+      }
+}
