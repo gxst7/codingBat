1195a5464da1955227992b9612c68e3ec050777c
Alexander Gostilo <alexandergostilo@gmail.com>
2023-09-30 01:24:18 +0300

    add notString problem

---
+++ java/warmup1/notString.java
@@ -0,0 +1,19 @@
+package java.warmup1;
+
+/**
+ * Warmup-1 > notString 
+ * 
+ * Given a string, return a new string where "not " has been added to the front. However, if 
+ * the string already begins with "not", return the string unchanged. Note: use .equals() to 
+ * compare 2 strings.
+ * 
+ * notString("candy") → "not candy"
+ * notString("x") → "not x"
+ * notString("not bad") → "not bad"
+ */
+
+public class notString {
+    public String notString(String str) {
+        return str.startsWith("not") ? str : "not " + str;
+    }
+}
