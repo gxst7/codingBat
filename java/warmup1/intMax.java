701ce90df2bcbd1a4efe1a3aa8b80463feeb4c6f
Alexander Gostilo <alexandergostilo@gmail.com>
2023-09-30 11:56:06 +0300

    add intMax problem

---
+++ java/warmup1/intMax.java
@@ -0,0 +1,24 @@
+package java.warmup1;
+
+/**
+ * Warmup-1 > intMax 
+ * 
+ * Given three int values, a b c, return the largest.
+ * 
+ * intMax(1, 2, 3) → 3
+ * intMax(1, 3, 2) → 3
+ * intMax(3, 2, 1) → 3
+ */
+
+public class intMax {
+    public int intMax(int a, int b, int c) {
+        int largest = a;
+        if (b > largest) {
+          largest = b;
+        }
+        if (c > largest) {
+          largest = c;
+        }
+        return largest;
+    }
+}
